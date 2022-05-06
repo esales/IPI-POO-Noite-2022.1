9850cacf1604d4441ba5c70c1599ca9622dd4eb9
esales <ericsales0@gmail.com>
2022-05-06 16:02:31 -0300

    Aula 06

---
+++ Aula06/src/Exemplo01.java
@@ -0,0 +1,31 @@
+public class Exemplo01 {
+
+	public static void main(String[] args) {
+//		int[] vetor;
+//		vetor = new int[20];
+//		
+//		vetor[0] = 50;
+//		vetor[1] = 75;
+//		vetor[2] = 100;
+//		
+//		System.out.println( vetor[0] );
+//		System.out.println( vetor[1] );
+//		System.out.println( vetor[2] );
+//		System.out.println( vetor[3] );
+//		
+//		vetor[1] = 100;
+//		
+//		System.out.println("");
+//		System.out.println( vetor[0] );
+//		System.out.println( vetor[1] );
+//		System.out.println( vetor[2] );
+//		System.out.println( vetor[3] );
+		
+		int[] vetor2 = {1, 2, 3, 4, 5};
+		System.out.println( vetor2[0] );
+		System.out.println( vetor2[1] );
+		System.out.println( vetor2[2] );
+		System.out.println( vetor2[3] );
+		
+	}
+}
