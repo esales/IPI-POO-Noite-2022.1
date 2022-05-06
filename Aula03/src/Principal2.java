5d6242ea988d9c0457917fca0a21a3516e65b288
esales <ericsales0@gmail.com>
2022-05-06 15:34:13 -0300

    Aula 03

---
+++ Aula03/src/Principal2.java
@@ -0,0 +1,9 @@
+
+public class Principal2 {
+
+	public static void main(String[] args) {
+		Veiculo veiculo = new Veiculo();
+		
+		System.out.println(veiculo.numeroRodas);
+	}
+}
--- Aula03/src/Veiculo.java
+++ Aula03/src/Veiculo.java
@@ -4,10 +4,28 @@ public class Veiculo {
 	String cor;
 	int velocidade;
 	int numeroRodas;
+	boolean temArCondicionado;
+	
+	public Veiculo() {
+		this.cor = "";
+		this.numeroRodas = 4;
+		this.velocidade = 0;
+		this.temArCondicionado = false;
+	}
 	
 	void acelerar(int incremento) {
 		if(velocidade + incremento <= 220) {
 			velocidade += incremento;
+		} else {
+			velocidade = 220;
+		}
+	}
+	
+	void frear(int decremento) {
+		if (velocidade - decremento >= 0) {
+			velocidade -= decremento;
+		} else {
+			velocidade = 0;
 		}
 	}
 }
