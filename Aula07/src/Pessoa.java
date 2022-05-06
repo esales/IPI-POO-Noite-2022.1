1c8947aacdaf309181e7ce758cd13bca2cd31d1c
esales <ericsales0@gmail.com>
2022-05-05 21:32:54 -0300

    Aula 07

---
+++ Aula07/src/Pessoa.java
@@ -0,0 +1,21 @@
+
+public class Pessoa {
+	private String nome;
+	private int idade;
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+}
---
+++ Aula07/src/Principal.java
@@ -0,0 +1,12 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Pessoa pessoa = new Pessoa();
+		
+		pessoa.setNome("Teste");
+		
+		System.out.println(pessoa.getNome());
+	}
+
+}
